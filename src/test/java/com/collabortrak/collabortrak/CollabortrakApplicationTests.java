f754b545e9be54075bdd5b4c5816a17b921e7fc0
leonredman <leonproart@yahoo.com>
2025-04-01 01:08:04 -0700

    Updating Application properties files for deployment

---
+++ src/test/java/com/collabortrak/collabortrak/CollabortrakApplicationTests.java
@@ -0,0 +1,10 @@
+package com.collabortrak.collabortrak;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+public class CollabortrakApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
