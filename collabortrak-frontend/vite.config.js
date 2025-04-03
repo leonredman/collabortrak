48ba74e607702a2ca27106f72540502cbe9be75d
leonredman <leonproart@yahoo.com>
2025-04-02 21:11:53 -0700

    Update Frontend with redirects file, headers file and update vite.config for netlify deploy

--- collabortrak-frontend/vite.config.js
+++ collabortrak-frontend/vite.config.js
@@ -4,4 +4,15 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+  base: "/",
 });
